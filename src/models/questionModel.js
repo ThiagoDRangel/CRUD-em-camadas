6537808182960d35558775c7adef5e370ab7421d
ThiagoDRangel <info.tec.campos@gmail.com>
2023-04-26 12:07:32 -0300

    Finalizando testes da camada model

--- src/models/questionModel.js
+++ src/models/questionModel.js
@@ -4,6 +4,8 @@ const getAll = async () => {
   const [questions] = await connection.execute(
     'SELECT id, question, user_id AS userId FROM questions;'
   );
+  // console.log('O que mockar no executa', questions);
+  // Executar o Thunder Client para pegar a estrutura do mock
   return questions;
 };
 
@@ -12,6 +14,7 @@ const create = async ({ question }) => {
     'INSERT INTO questions (question) VALUES (?);',
     [question],
   );
+  console.log('mock do create', insertId);
   return { id: insertId, question };
 };
 
